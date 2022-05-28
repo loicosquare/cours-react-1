6526fcbbaa6923c268c0b37eb7c0a79c6c1dd644
loico square <loicsan07@gmail.com>
2022-05-28 04:04:47 +0100

    Creating component and adding style.css

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Libero quos at quo voluptatem deserunt animi ad quae nesciunt aliquid alias minus quidem, unde quia vero laborum? Odio molestiae incidunt, reprehenderit illum ex ipsa dignissimos culpa quaerat, animi nisi necessitatibus optio nulla dicta aperiam pariatur. Porro ea veniam sapiente nihil voluptatum atque culpa neque perferendis, praesentium assumenda, adipisci corrupti. Optio inventore enim, ipsum adipisci sed repellendus! Laborum esse, repellendus quia nostrum nobis accusantium rerum laudantium debitis aliquam maxime! Ipsum cupiditate voluptas et tempora rem eaque delectus voluptatibus odit, aperiam inventore magnam distinctio est architecto nam deleniti atque minima alias culpa consectetur, vel saepe id! Consequatur, nostrum iusto explicabo beatae ipsum sed sit qui tempore ducimus eum autem porro magni necessitatibus obcaecati aliquam voluptates? Porro architecto dolorem officia fuga unde eligendi ipsa id quae, quas tempora aspernatur. Recusandae dolor maiores quibusdam vero! Natus, animi? Obcaecati officia eius cupiditate porro, itaque illo sequi dolorum facere, consectetur dolorem optio perferendis nobis iure accusantium eum a consequuntur deleniti ex distinctio magni modi. Ullam a dolor veniam nemo autem perspiciatis pariatur nisi inventore accusamus doloremque, repudiandae incidunt laudantium reprehenderit facere nesciunt enim ratione adipisci, magni explicabo! Quis cum quasi porro quidem atque iusto facere quo inventore.</p>
+        </div>
+    );
+};
+
+export default About;
